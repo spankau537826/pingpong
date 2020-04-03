ec46e9fecf6d38e09f76f6d3883aea8b838c6780
duygub26 <duygu.boztuna@hotmail.de>
2020-04-03 14:55:39 +0200

    create menu.css, create 'Anleitung' button in index.html, add css-style from the buttons in menu.css. Ich habe alle drei css Dokumente auskommentiert im index.html

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,11 @@
 var einspielerBtn = document.getElementById('einspielerBtn');
 var zweispielerBtn = document.getElementById('zweispielerBtn');
 var ubungBtn = document.getElementById('ubungBtn');
+var anleitungBtn = document.getElementById('anleitungBtn');
 
 function initialize(){
     einspielerBtn.style.display = 'none';
     zweispielerBtn.style.display = 'none';
     ubungBtn.style.display = 'none';
+
 }
