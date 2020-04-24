39ab57f2287843271e361ce61e5fc7c35fecdf62
duygub26 <duygu.boztuna@hotmail.de>
2020-04-24 10:27:16 +0200

    Style 'Anleitung' button and the other buttons. Add 'box' and 'close' to menu.css to style the alert box. Change font-family in normalize.css to Montserrat. Add 'function pop()' to menu.js. Add 'close' button to index.html and i add all buttons in a div tag.

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,20 @@
 var einspielerBtn = document.getElementById('einspielerBtn');
 var zweispielerBtn = document.getElementById('zweispielerBtn');
 var ubungBtn = document.getElementById('ubungBtn');
-var anleitungBtn = document.getElementById('anleitungBtn');
+var c = 0;
 
 function initialize(){
     einspielerBtn.style.display = 'none';
     zweispielerBtn.style.display = 'none';
     ubungBtn.style.display = 'none';
+}
 
+function pop(){
+    if(c == 0){
+        document.getElementById("box").style.display = "block";
+        c = 1;
+    }else{
+        document.getElementById("box").style.display = "none";
+        c = 0;
+    }
 }
