0934a9e5de1dd562d606b27141d7dc0bad48a607
lozomar <m.lozova@drooms.com>
2020-05-11 22:50:04 +0200

    1. created game over menu, added game over picture, updated drawScore method 2. changed anleitung popup size, changed font and color 3. added link to the github from anleitung 4. added box shadow for field, removed border color

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,18 @@
 function initialize() {
     menu.style.display = 'none';
     speedMenu.style.display = 'none';
+    gameOver.style.display = 'none';
+
     field.style.filter = "blur(0px)";
     speedBtn.style.display = "block";
     homeIcon.style.display = "block";
     playIcon.style.display = "block";
+    restartIcon.style.display = "none";
+
     speedBtn.style.filter = "blur(0px)";
     homeIcon.style.filter = "blur(0px)";
     playIcon.style.filter = "blur(0px)";
+    restartIcon.style.filter = "blur(0px)";
 }
 
 let modalHidden = true;
