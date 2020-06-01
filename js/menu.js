80dadba538e62dca89b84ffa054cf879853434d8
spankau537826 <spankau@applabs.de>
2020-06-01 18:16:36 +0200

    create session id when connection is established

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,5 @@
+var isMultiplayerSession =  Boolean(false);
+
 function initialize() {
     menu.style.display = 'none';
     speedMenu.style.display = 'none';
@@ -15,6 +17,24 @@ function initialize() {
     restartIcon.style.filter = "blur(0px)";
 }
 
+function initializeMultiplayer() {
+    menu.style.display = 'none';
+    speedMenu.style.display = 'none';
+    gameOver.style.display = 'none';
+
+    field.style.filter = "blur(0px)";
+    speedBtn.style.display = "block";
+    homeIcon.style.display = "block";
+    playIcon.style.display = "block";
+    restartIcon.style.display = "none";
+
+    speedBtn.style.filter = "blur(0px)";
+    homeIcon.style.filter = "blur(0px)";
+    playIcon.style.filter = "blur(0px)";
+    restartIcon.style.filter = "blur(0px)";
+    isMultiplayerSession = Boolean(true);
+}
+
 let modalHidden = true;
 function pop() {
     const modalId = document.getElementById("anleitung");
@@ -28,6 +48,10 @@ function pop() {
 }
 
 var socket = io();
+socket.emit('new player');
 socket.on('message', function(data) {
   console.log(data);
+});
+socket.on('servererror',function(err) {
+    window.alert("Es konnte keine Session erstellt werden. Bitte schließen Sie das Fenster und versuchen Sie es erneut.");
 });
--- server.js
+++ server.js
@@ -23,10 +23,101 @@ server.listen(PORT, function() {
   console.log(`Starting server on port ${PORT}`);
 });
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: 'postgres://jzhvuxxodzvrxu:1b404a5c1bd2698a0f24309580cbcbf313c39bb4c192cfa69f04d4f618d8e095@ec2-46-137-156-205.eu-west-1.compute.amazonaws.com:5432/daq5jiheq0aea7',
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+var players = {};
+
 // Add the WebSocket handlers
 io.on('connection', function(socket) {
-  console.log('client connected');
+  console.log('Client connected');
+  const sessionid = uuidv4();
+  socket.on('new player', function() {
+    console.log('new player');
+    // players[socket.id] = {
+    //   x: 300,
+    //   y: 300
+    // };
+    client.query(`INSERT INTO session (id) VALUES ('${sessionid}');`, (err, res) => {
+      console.log(sessionid);
+      if (err) {
+        socket.emit('servererror',err);
+        if (err.code !== undefined) {
+          console.log("pg error code:", err.code);
+        
+          // 42601 = 'syntax_error'
+          if (err.code === "42601") {
+            // return the position of the SQL syntax error
+            console.log("SQL syntax error position:", err.position);
+          }
+        }
+        throw err;
+      }
+      for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+      }
+      socket.emit('sessionid',sessionid);
+      // client.end();
+    });
+  });
+  socket.on('disconnect', function() {
+    console.log('Client disconnected');
+    client.query(`DELETE FROM session WHERE id='${sessionid}';`, (err, res) => {
+      if (err) {
+        socket.emit('servererror',err);
+        if (err.code !== undefined) {
+          console.log("pg error code:", err.code);
+        
+          // 42601 = 'syntax_error'
+          if (err.code === "42601") {
+            // return the position of the SQL syntax error
+            console.log("SQL syntax error position:", err.position);
+          }
+        }
+        throw err;
+      }
+    });
+  })
 });
+
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+function sendQuery(query) {
+  var result = false;
+  client.query(query, (err, res) => {
+    if (err) {
+      socket.emit('servererror',err);
+      if (err.code !== undefined) {
+        console.log("pg error code:", err.code);
+      
+        // 42601 = 'syntax_error'
+        if (err.code === "42601") {
+          // return the position of the SQL syntax error
+          console.log("SQL syntax error position:", err.position);
+        }
+      }
+      throw err;
+    } 
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    result = true;
+  });
+  return Boolean(result);
+}
 
 // var express = require("express");
 // var socketIO = require("socket.io");
@@ -56,16 +147,7 @@ io.on('connection', function(socket) {
 //   console.log(`Listening on ${PORT}`);
 // });
 
-const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: 'postgres://jzhvuxxodzvrxu:1b404a5c1bd2698a0f24309580cbcbf313c39bb4c192cfa69f04d4f618d8e095@ec2-46-137-156-205.eu-west-1.compute.amazonaws.com:5432/daq5jiheq0aea7',
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-client.connect();
 
 // client.query('SELECT * FROM session', (err, res) => {
 //   if (err) throw err;
